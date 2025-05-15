8e78700eeb4d7841908a750c0280387aa20f8ffe
omprakashdebnath <debnathomprakash91@gmail.com>
2025-05-15 12:27:28 +0530

    end

--- changeitem.py
+++ changeitem.py
@@ -38,3 +38,16 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 for x in thislist:
     print(x)
+thislist = ["apple", "banana", "crerry"]
+for i in range(len(thislist)):
+    print(thislist[i])
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+    print(thislist[i])
+    i = i + 1
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+thislist = ["apple", "banana", "cherry"]
+[print(thislist)]
---
+++ listcomparision.py
@@ -0,0 +1,1 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
