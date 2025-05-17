a5cb93ceba66fc556ce59b4a77dc001d9ba4b2e9
omprakashdebnath <debnathomprakash91@gmail.com>
2025-05-17 19:39:05 +0530

    finished

---
+++ tuple.py
@@ -0,0 +1,37 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+thistuple = ("apple", "banana", "cherry", "mango")
+print(len(thistuple))
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#not a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+thistuple = (("apple", "banana", "cherry")) #note the double round brackets
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
