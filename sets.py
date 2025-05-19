6488ddca3662ecba7b01a98ce3ebcf5063a8aeb7
omprakashdebnath <debnathomprakash91@gmail.com>
2025-05-19 10:48:30 +0530

    next is join set

---
+++ sets.py
@@ -0,0 +1,73 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+thisset = {"apple", "banana", "cherry", False, True, 0}
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+  thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
